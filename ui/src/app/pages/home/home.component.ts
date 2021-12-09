e6a1f50ea90f6acbac878966be1c66194bddf64f
ruchianu <anuenglishacademy@gmail.com>
2021-12-09 20:24:37 +0530

    video on home

--- ui/src/app/pages/home/home.component.ts
+++ ui/src/app/pages/home/home.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import Player from "@vimeo/player";
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  safeSrc:any
+  registerSucess=false
+  //constructor() { }
+  constructor(    
+    private sanitizer: DomSanitizer,
+    private router: Router) {
+      console.log("Video file being loaded");
+    this.registerSucess = false
+    this.safeSrc = this.sanitizer.bypassSecurityTrustResourceUrl("https://player.vimeo.com/video/635389651?h=406f39766e");                      
+    //https://player.vimeo.com/video/637204860?h=edf9f43fca&amp
+    //https://player.vimeo.com/video/635389651?h=406f39766e
   }
 
+  ngOnInit(): void {
+    var handstickPlayer = new Player('handstick');
+    handstickPlayer.on('play', function() {
+        console.log('played the handstick video!');
+    });
+  }  
+  public myfunction(message:string){
+    //onLoad();
+    alert(message);    
+  }
 }
